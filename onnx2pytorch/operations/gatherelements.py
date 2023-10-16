365f481f71114c82542604838af8d1adb422cd76
Siim Haugas <haugas@gmail.com>
2023-10-16 22:05:05 +0300

    Update gatherelements.py

--- onnx2pytorch/operations/gatherelements.py
+++ onnx2pytorch/operations/gatherelements.py
@@ -3,7 +3,19 @@ from torch import nn
 
 class GatherElements(nn.Module):
     def forward(self, data, indices, axis=0):
-        # We must roll axis to first dim which is what PyTorch's gather expects
+        # Ensure axis is a valid dimension
+        if not (0 <= axis < data.dim()):
+            raise ValueError(("Expected axis to be an integer between 0 and the number "
+                              "of dimensions of input tensor, but got axis {} "
+                              "for input of dimension {}").format(axis, data.dim()))
+
+        # Ensure data and indices have the same dimensions
+        if data.dim() != indices.dim():
+            raise ValueError(("Expected data and indices to have the same number of "
+                              "dimensions, but got {} and {}, respectively.")
+                             .format(data.dim(), indices.dim()))
+
+        # Roll the axis to be gathered to the front of the tensor
         data = data.roll(-axis, range(data.dim()))
         indices = indices.roll(-axis, range(indices.dim()))
 
@@ -12,4 +24,5 @@ class GatherElements(nn.Module):
 
         # Return axes to their original order
         gathered_elements = gathered_elements.roll(axis, range(gathered_elements.dim()))
+        
         return gathered_elements
